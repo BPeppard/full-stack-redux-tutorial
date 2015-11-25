8f90007c31e8c2714dcc61b2d7b0f5d251a63a2a
unknown <peppardb@IT-4S03P22.corwinnet.city.cor.gov>
2015-11-25 08:25:32 -0600

    adds next function to handle getting two entries to vote on

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,12 +1,11 @@
 import {List, Map} from 'immutable'
 import {expect} from 'chai'
 
-import {setEntries} from '../src/core'
+import {setEntries, next} from '../src/core'
 
 describe('application logic', () => {
 
   describe('setEntries', () => {
-
     it('adds the entries to the state', () => {
       const state = Map()
       const entries = List.of('Trainspotting', '28 Days Later')
@@ -15,7 +14,6 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }))
     })
-
     it('converts to immutable', () => {
       const state = Map()
       const entries = ['Trainspotting', '28 Days Later']
@@ -24,7 +22,21 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }))
     })
+  })
 
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      })
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }))
+    })
   })
 
 })
